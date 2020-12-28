795fd460278a461cc04145938d87c61309352455
jhohan alexander vasquez <jhohan25@hotmail.com>
2020-12-27 21:47:18 -0500

    survey Controllers

--- App/Controllers/surveyFormCtrl.js
+++ App/Controllers/surveyFormCtrl.js
@@ -1,5 +1,5 @@
 ﻿angular
-    .module('surveyControllers', [])
+    .module('surveyControllers', ['ngAnimate'])
     .controller('surveyFormCtrl', ['$scope', '$http', 'questionsFactory', 'answersFactory', '$location',
         function ($scope, $http, questionsFactory, answersFactory, $location, $sce) {
 
@@ -10,35 +10,11 @@
                     console.log(data);
                 });
 
-            answersFactory.callAnswersData()
-                .then(function (data) {
-                    $scope.answers = angular.fromJson(data);
-                }, function (data) {
-                    console.log(data);
-                });
-
-            // $scope.answers = 'Survey Questions go here.';
-
-            // Wrap in function to be able to load 
-            // after new question has been asked
-            $scope.loadAnswers = function () {
-                answersFactory.callAnswersData()
-                    .then(function (data) {
-                        $scope.answers = angular.fromJson(data);
-                    }, function (data) {
-                        console.log(data);
-                    });
-            };
-            $scope.loadAnswers();
-
-            $scope.addNewAnswer = function (newAnswer) {
+            $scope.addNewAnswer = function (newAnswer, newQuestion) {
                 function addAnswer(newAnswerName, newQuestionId) {
                     answersFactory.setAnswers(newAnswerName, newQuestionId)
                         .success(function () {
                             $scope.status = 'Inserted Answer! Refreshing Answer list.';
-                            // Reload Answers - pulls list from API to load Answers that
-                            // could be added or removed by other users since page has been loaded
-                            $scope.loadAnswers();
                         }).
                         error(function (error) {
                             $scope.status = 'Unable to insert Answer: ' + error.message;
@@ -54,7 +30,7 @@
                 }
 
                 // Create new object that will be passed to API
-                var NewAnswer = new Answer(newAnswer);
+                var NewAnswer = new Answer(newAnswer, newQuestion);
                 // Pass new Answer object to API
                 addAnswer(NewAnswer);
                 // Clear out new Answer input
--- App/Controllers/surveyResultsCtrl.js
+++ App/Controllers/surveyResultsCtrl.js
@@ -1,6 +1,12 @@
 ﻿angular
-	.module('surveyControllers')
-	.controller('surveyResultsCtrl', ['$scope', '$http', '$location',
-	function ($scope, $http, $location, $sce) {
-		$scope.questions = "Survey Results go here (pending  module).";
-	}]);
+    .module('surveyControllers', [])
+    .controller('surveyResultsCtrl', ['$scope', '$http', 'answersFactory', '$location',
+        function ($scope, $http, answersFactory, $location, $sce) {
+
+            answersFactory.callAnswersData()
+                .then(function (data) {
+                    $scope.answers = angular.fromJson(data);
+                }, function (data) {
+                    console.log(data);
+                });
+        }]);
