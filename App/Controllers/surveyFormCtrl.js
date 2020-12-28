7962d57736930ad1d0aa0221efc065e12762675f
jhohan alexander vasquez <jhohan25@hotmail.com>
2020-12-27 19:50:21 -0500

    Script DataBase DNNModuleTest

--- App/Controllers/surveyFormCtrl.js
+++ App/Controllers/surveyFormCtrl.js
@@ -1,7 +1,7 @@
 ﻿angular
 	.module('surveyControllers', [])
 	.controller('surveyFormCtrl', ['$scope', '$http', 'questionsFactory', 'answersFactory', '$location',
-	function ($scope, $http, questionsFactory, answersFactory, $location, $sce) {
+		function ($scope, $http, questionsFactory, answersFactory, $location, $sce) {
 
 		questionsFactory.callQuestionsData()
 			.then(function (data) {
@@ -15,8 +15,50 @@
 		 		$scope.answers = angular.fromJson(data);
 		 	}, function (data) {
 		 		console.log(data);
-		 	});
+			  });
 
 		// $scope.answers = 'Survey Questions go here.';
 
+		// Wrap in function to be able to load 
+		// after new question has been asked
+		$scope.loadAnswers = function () {
+			answersFactory.callAnswersData()
+				.then(function (data) {
+					$scope.Answers = angular.fromJson(data);
+				}, function (data) {
+					console.log(data);
+				});
+		};
+		$scope.loadAnswers();
+
+		$scope.addNewAnswer = function (newAnswer) {
+			function addAnswer(newAnswerName) {
+				answersFactory.setAnswers(newAnswerName)
+					.success(function () {
+						$scope.status = 'Inserted Answer! Refreshing Answer list.';
+						// Reload Answers - pulls list from API to load Answers that
+						// could be added or removed by other users since page has been loaded
+						$scope.loadAnswers();
+					}).
+					error(function (error) {
+						$scope.status = 'Unable to insert Answer: ' + error.message;
+					});
+			}
+
+			// Answer object constructor
+			// Properties are the same as what the AnswerController is expecting
+			function Answer(name) {
+				// Name from input
+				this.Name = name;
+				// Static for now, can add variations later
+				this.Description = 'Text';
+			}
+
+			// Create new object that will be passed to API
+			var NewAnswer = new Answer(newAnswer);
+			// Pass new Answer object to API
+			addAnswer(NewAnswer);
+			// Clear out new Answer input
+			$scope.newAnswerName = '';
+		};
 	}]);
---
+++ DB/ScriptSurveyDB.sql
@@ -0,0 +1,92 @@
+USE [DotNetNuke]
+GO
+
+/****** Object:  Table [dbo].[DNNModuleTest_Answers]    Script Date: 27/12/2020 07:48:14 p. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DNNModuleTest_Answers](
+	[AnswerId] [int] IDENTITY(1,1) NOT NULL,
+	[AnswerValue] [int] NULL,
+	[QuestionId] [int] NULL,
+	[AssignedUserId] [int] NULL,
+	[ModuleId] [int] NULL,
+	[CreatedByUserId] [int] NULL,
+	[LastModifiedByUserId] [int] NULL,
+	[CreatedOnDate] [datetime] NULL,
+	[LastModifiedOnDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[AnswerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[DNNModuleTest_Answers]  WITH CHECK ADD FOREIGN KEY([AnswerId])
+REFERENCES [dbo].[DNNModuleTest_Answers] ([AnswerId])
+GO
+
+
+USE [DotNetNuke]
+GO
+
+/****** Object:  Table [dbo].[DNNModuleTest_Items]    Script Date: 27/12/2020 07:48:49 p. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DNNModuleTest_Items](
+	[ItemId] [int] IDENTITY(1,1) NOT NULL,
+	[ItemName] [nvarchar](max) NOT NULL,
+	[ItemDescription] [nvarchar](max) NOT NULL,
+	[AssignedUserId] [int] NULL,
+	[ModuleId] [int] NOT NULL,
+	[CreatedOnDate] [datetime] NOT NULL,
+	[CreatedByUserId] [int] NOT NULL,
+	[LastModifiedOnDate] [datetime] NOT NULL,
+	[LastModifiedByUserId] [int] NOT NULL,
+ CONSTRAINT [PK_DNNModuleTest_Items] PRIMARY KEY CLUSTERED 
+(
+	[ItemId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+USE [DotNetNuke]
+GO
+
+/****** Object:  Table [dbo].[DNNModuleTest_Questions]    Script Date: 27/12/2020 07:49:06 p. m. ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[DNNModuleTest_Questions](
+	[QuestionId] [int] IDENTITY(1,1) NOT NULL,
+	[QuestionName] [varchar](max) NULL,
+	[QuestionType] [varchar](max) NULL,
+	[AssignedUserId] [int] NULL,
+	[ModuleId] [int] NULL,
+	[CreatedByUserId] [int] NULL,
+	[LastModifiedByUserId] [int] NULL,
+	[CreatedOnDate] [datetime] NULL,
+	[LastModifiedOnDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[QuestionId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[DNNModuleTest_Questions]  WITH CHECK ADD FOREIGN KEY([QuestionId])
+REFERENCES [dbo].[DNNModuleTest_Questions] ([QuestionId])
+GO
+
+
